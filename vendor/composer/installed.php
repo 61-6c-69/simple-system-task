77fe5d7de7eaf2a739ef30a699bd681086805571
ali <alifarshad13@gmail.com>
2022-05-07 13:10:57 +0430

    add vendor and update table.sql

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'ali/simple-system',
+  ),
+  'versions' => 
+  array (
+    'ali/simple-system' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'catfan/medoo' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '72c6d56e1cccbdb5e2bb1851604618bb88f4d9ca',
+    ),
+  ),
+);
